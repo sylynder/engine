8a9a4b3968411fa723937cfe940182368ca714f8
otengkwame <developerkwame@gmail.com>
2022-12-31 18:18:50 +0000

    :zap: bump sylynder/engine v1.8.0 to v2.0.0
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- Core/core/Console/Console.php
+++ Core/core/Console/Console.php
@@ -26,7 +26,7 @@ class Console
 
     private static $composerCommand = 'composer ';
 
-    private const WEBBY_CLI_VERSION = '1.8.0';
+    private const WEBBY_CLI_VERSION = '2.0.0';
 
     private const DEFAULT_HOST = "localhost";
     
