a043fe68dc4e16a07defc51c6243b357dc7d17af
otengkwame <developerkwame@gmail.com>
2023-06-07 08:29:33 +0000

    :bug: fix form_validation not found in the valid_password() method
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- CodeIgniter/Framework/libraries/Form_validation.php
+++ CodeIgniter/Framework/libraries/Form_validation.php
@@ -2123,49 +2123,49 @@ class CI_Form_validation
 
 		if (empty($password)) {
 			
-			$this->form_validation->set_message('valid_password', 'The {field} field is required.');
+			$this->set_message('valid_password', 'The {field} field is required.');
 
 			return false;
 		}
 
 		if (preg_match_all($regex_lowercase, $password) < 1) {
 			
-			$this->form_validation->set_message('valid_password', 'The {field} field must be at least one lowercase letter.');
+			$this->set_message('valid_password', 'The {field} field must be at least one lowercase letter.');
 
 			return false;
 		}
 
 		if (preg_match_all($regex_uppercase, $password) < 1) {
 			
-			$this->form_validation->set_message('valid_password', 'The {field} field must be at least one uppercase letter.');
+			$this->set_message('valid_password', 'The {field} field must be at least one uppercase letter.');
 
 			return false;
 		}
 
 		if (preg_match_all($regex_number, $password) < 1) {
 			
-			$this->form_validation->set_message('valid_password', 'The {field} field must have at least one number.');
+			$this->set_message('valid_password', 'The {field} field must have at least one number.');
 
 			return false;
 		}
 
 		if (preg_match_all($regex_special, $password) < 1) {
 			
-			$this->form_validation->set_message('valid_password', 'The {field} field must have at least one special character.' . ' ' . htmlentities('!@#$%^&*()\-_=+{};:,<.>§~'));
+			$this->set_message('valid_password', 'The {field} field must have at least one special character.' . ' ' . htmlentities('!@#$%^&*()\-_=+{};:,<.>§~'));
 
 			return false;
 		}
 
 		if (strlen($password) < 5) {
 			
-			$this->form_validation->set_message('valid_password', 'The {field} field must be at least 5 characters in length.');
+			$this->set_message('valid_password', 'The {field} field must be at least 5 characters in length.');
 
 			return false;
 		}
 
 		if (strlen($password) > 32) {
 			
-			$this->form_validation->set_message('valid_password', 'The {field} field cannot exceed 32 characters in length.');
+			$this->set_message('valid_password', 'The {field} field cannot exceed 32 characters in length.');
 
 			return false;
 		}
